38d85adb39a34af79b7def4dce351acc7cf139df
Vlad <vladka@webdevelop.pro>
2025-05-20 14:16:07 +0200

    add evm models

---
+++ evms/consts.go
@@ -0,0 +1,38 @@
+package fundingsources
+
+import "github.com/pkg/errors"
+
+type WalletStatusT string
+
+// Enum values for WalletStatusT
+const (
+	WalletStatusTCreated  WalletStatusT = "created"
+	WalletStatusTPending  WalletStatusT = "pending"
+	WalletStatusTVerified WalletStatusT = "verified"
+	WalletStatusTError    WalletStatusT = "error"
+
+	AppLabel  = "wallet"
+	ModelName = "fundingsource"
+	TableName = "evm_wallets"
+)
+
+func AllWalletStatusT() []WalletStatusT {
+	return []WalletStatusT{
+		WalletStatusTCreated,
+		WalletStatusTVerified,
+		WalletStatusTError,
+	}
+}
+
+func (e WalletStatusT) IsValid() error {
+	switch e {
+	case WalletStatusTCreated, WalletStatusTVerified, WalletStatusTError:
+		return nil
+	default:
+		return errors.New("enum is not valid")
+	}
+}
+
+func (e WalletStatusT) String() string {
+	return string(e)
+}
