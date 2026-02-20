0b860e5afe6a38d2707f860a929ebe0f7b0384ec
Vlad <vladka@webdevelop.pro>
2026-02-20 19:49:13 +0100

    updated filers consts

--- filers/consts.go
+++ filers/consts.go
@@ -1,13 +1,17 @@
 package filers
 
-type OfferFileType string
+type FolderName string
+type Type string
 
 const (
-	OfferTypeCompany              OfferFileType = "company"
-	OfferTypeTax                  OfferFileType = "tax"
-	OfferTypeInvestmentAgreements OfferFileType = "investment-agreements"
-	OfferTypeInvestorUpdates      OfferFileType = "investor-updates"
-	OfferTypeOther                OfferFileType = "other"
+	FolderCompany              FolderName = "company"
+	FolderTax                  FolderName = "tax"
+	FolderInvestmentAgreements FolderName = "investment_agreements"
+	FolderInvestorUpdates      FolderName = "investor_updates"
+	FolderOther                FolderName = "other"
+
+	TypeFile   Type = "file"
+	TypeFolder Type = "folder"
 
 	AppLabel  = "filer_ltree"
 	ModelName = "filerltree"
